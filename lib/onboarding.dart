84aeef31780ed169895207503bd5492ae6d77b13
nico <90756549+sengokus@users.noreply.github.com>
2024-06-09 10:44:39 +0800

    Overhauled onboarding

--- lib/onboarding.dart
+++ lib/onboarding.dart
@@ -1,111 +1,113 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_onboarding_slider/flutter_onboarding_slider.dart';
+import 'package:weatherup/components/sliderpage.dart';
 
-class OnboardingPage extends StatelessWidget {
+class OnboardingPage extends StatefulWidget {
   const OnboardingPage({super.key});
 
   @override
+  State<OnboardingPage> createState() => _OnboardingPageState();
+}
+
+class _OnboardingPageState extends State<OnboardingPage> {
+  int _currentPage = 0;
+  late final PageController _controller = PageController();
+
+  final List<Widget> _pages = [
+    const SliderPage(
+        title: "Keep",
+        description:
+            "Accept cryptocurrencies and digital assets, keep thern here, or send to orthers",
+        image: "lib/assets/image1.jpg"),
+    const SliderPage(
+        title: "Buy",
+        description:
+            "Buy Bitcoin and cryptocurrencies with VISA and MasterVard right in the App",
+        image: "lib/assets/image2.jpg"),
+    const SliderPage(
+        title: "Sell",
+        description:
+            "Sell your Bitcoin cryptocurrencies or Change with orthres digital assets or flat money",
+        image: "lib/assets/image3.jpg"),
+  ];
+
+  _onchanged(int index) {
+    setState(() {
+      _currentPage = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return OnBoardingSlider(
-        totalPage: 3,
-        headerBackgroundColor: Theme.of(context).primaryColor,
-        pageBackgroundColor: Theme.of(context).primaryColor,
-        skipIcon: Icon(Icons.keyboard_arrow_right,
-            color: Theme.of(context).buttonTheme.colorScheme?.secondary),
-        finishButtonText: 'Get Started',
-        finishButtonStyle: FinishButtonStyle(
-            backgroundColor: Theme.of(context).buttonTheme.colorScheme?.primary,
-            splashColor: Theme.of(context).splashColor),
-        controllerColor: Theme.of(context).buttonTheme.colorScheme?.secondary,
-        speed: 1.8,
-        background: [
-          Image.asset('lib/assets/image3.jpg', height: 400),
-          Image.asset('lib/assets/image3.jpg', height: 400),
-          Image.asset('lib/assets/image3.jpg', height: 400),
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          PageView.builder(
+            scrollDirection: Axis.horizontal,
+            onPageChanged: _onchanged,
+            controller: _controller,
+            itemCount: _pages.length,
+            itemBuilder: (context, int index) {
+              return _pages[index];
+            },
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              // Generate the page indicator
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: List<Widget>.generate(_pages.length, (int index) {
+                    return AnimatedContainer(
+                        duration: const Duration(milliseconds: 300),
+                        height: 10,
+                        width: (index == _currentPage) ? 30 : 10,
+                        margin: const EdgeInsets.symmetric(
+                            horizontal: 5, vertical: 30),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            color: (index == _currentPage)
+                                ? Theme.of(context)
+                                    .buttonTheme
+                                    .colorScheme
+                                    ?.primary
+                                : Theme.of(context)
+                                    .buttonTheme
+                                    .colorScheme
+                                    ?.secondary));
+                  })),
+              InkWell(
+                onTap: () {
+                  _controller.nextPage(
+                      duration: const Duration(milliseconds: 800),
+                      curve: Curves.easeInOutQuint);
+                },
+                child: AnimatedContainer(
+                  alignment: Alignment.center,
+                  duration: const Duration(milliseconds: 300),
+                  height: 70,
+                  width: (_currentPage == (_pages.length - 1)) ? 200 : 70,
+                  decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(35)),
+                  child: (_currentPage == (_pages.length - 1))
+                      ? Text(
+                          "Get Started",
+                          style: Theme.of(context).textTheme.displayMedium,
+                        )
+                      : const Icon(
+                          Icons.navigate_next,
+                          size: 50,
+                          color: Colors.white,
+                        ),
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              )
+            ],
+          ),
         ],
-        centerBackground: true,
-        pageBodies: [
-          Container(
-            alignment: Alignment.center,
-            width: MediaQuery.of(context).size.width,
-            padding: const EdgeInsets.symmetric(horizontal: 40),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                const SizedBox(
-                  height: 480,
-                ),
-                Text(
-                  'On your way...',
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.displayMedium,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  'to find the perfect looking Onboarding for your app?',
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.displayMedium,
-                ),
-              ],
-            ),
-          ),
-          Container(
-            alignment: Alignment.center,
-            width: MediaQuery.of(context).size.width,
-            padding: const EdgeInsets.symmetric(horizontal: 40),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                const SizedBox(
-                  height: 480,
-                ),
-                Text(
-                  'On your way...',
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.displayMedium,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  'to find the perfect looking Onboarding for your app?',
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.displayMedium,
-                ),
-              ],
-            ),
-          ),
-          Container(
-            alignment: Alignment.center,
-            width: MediaQuery.of(context).size.width,
-            padding: const EdgeInsets.symmetric(horizontal: 40),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                const SizedBox(
-                  height: 480,
-                ),
-                Text(
-                  'On your way...',
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.displayMedium,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  'to find the perfect looking Onboarding for your app?',
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.displayMedium,
-                ),
-              ],
-            ),
-          ),
-        ]);
+      ),
+    );
   }
 }
