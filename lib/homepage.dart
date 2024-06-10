36e17722f44e3194e863cab9c42b7be9836a7d26
nico <90756549+sengokus@users.noreply.github.com>
2024-06-10 23:02:04 +0800

    Integrate OpenWeatherAPI with geolocation

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+
 import 'package:geolocator/geolocator.dart';
+import 'package:weather/weather.dart';
 
 import 'package:weatherup/components/background.dart';
+import 'package:weatherup/utils/const.dart';
 import 'package:weatherup/utils/geolocator.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,15 +15,26 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final WeatherFactory _wf = WeatherFactory(OPENWEATHER_API_KEY);
+
   Position? _currentPosition;
   String? _locationError;
+  Weather? _weather;
 
   @override
   void initState() {
     super.initState();
-    _fetchCurrentPosition();
+    _initWeather();
   }
 
+  Future<void> _initWeather() async {
+    await _fetchCurrentPosition();
+    if (_currentPosition != null) {
+      await _fetchWeather();
+    }
+  }
+
+  // Fetch the current position of the device
   Future<void> _fetchCurrentPosition() async {
     try {
       Position position = await determinePosition();
@@ -35,14 +49,29 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
+  // Fetch the weather data based on the current position
+  Future<void> _fetchWeather() async {
+    try {
+      Weather w = await _wf.currentWeatherByLocation(
+          _currentPosition!.latitude, _currentPosition!.longitude);
+      setState(() {
+        _weather = w;
+      });
+    } catch (e) {
+      setState(() {
+        _locationError = "Failed to fetch weather data: $e";
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return BuildBackground(
       children: <Widget>[
         Center(
-          child: _currentPosition != null
+          child: _currentPosition != null && _weather != null
               ? Text(
-                  "Latitude: ${_currentPosition!.latitude}\nLongitude: ${_currentPosition!.longitude}",
+                  "Latitude: ${_currentPosition!.latitude}\nLongitude: ${_currentPosition!.longitude}\nWeather: ${_weather?.areaName ?? ''}",
                   style: Theme.of(context).textTheme.displayMedium,
                 )
               : _locationError != null
---
+++ lib/utils/const.dart
@@ -0,0 +1,1 @@
+const OPENWEATHER_API_KEY = 'ccb7c1cf334ff3d4d26f8c8c50e17b5f';
