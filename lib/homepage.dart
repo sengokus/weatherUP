731f705aa64c85f9d3cd294b7d5b965f5ff33591
nico <90756549+sengokus@users.noreply.github.com>
2024-06-10 17:42:21 +0800

    Fetch current location by granting permissions

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 import 'package:weatherup/components/background.dart';
+import 'package:weatherup/utils/geolocator.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,19 +12,45 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Position? _currentPosition;
+  String? _locationError;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchCurrentPosition();
+  }
+
+  Future<void> _fetchCurrentPosition() async {
+    try {
+      Position position = await determinePosition();
+      setState(() {
+        _currentPosition = position;
+        _locationError = null;
+      });
+    } catch (e) {
+      setState(() {
+        _locationError = e.toString();
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const BuildBackground(
+    return BuildBackground(
       children: <Widget>[
         Center(
-          child: Text(
-            "Home Page",
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              color: Color.fromARGB(255, 233, 234, 235),
-            ),
-          ),
+          child: _currentPosition != null
+              ? Text(
+                  "Latitude: ${_currentPosition!.latitude}\nLongitude: ${_currentPosition!.longitude}",
+                  style: Theme.of(context).textTheme.displayMedium,
+                )
+              : _locationError != null
+                  ? Text(
+                      "Error: $_locationError",
+                      style: Theme.of(context).textTheme.displayMedium,
+                    )
+                  : const CircularProgressIndicator(),
         ),
       ],
     );
---
+++ lib/utils/geolocator.dart
@@ -0,0 +1,28 @@
+import 'package:geolocator/geolocator.dart';
+
+/// Determine the current position of the device
+Future<Position> determinePosition() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  // Test if location services are enabled
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    return Future.error('Location services are disabled');
+  }
+
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      return Future.error('Location permissions are denied');
+    }
+  }
+
+  if (permission == LocationPermission.deniedForever) {
+    return Future.error(
+        'Location permissions are permanently denied, permission cannot be granted');
+  }
+
+  return await Geolocator.getCurrentPosition();
+}
